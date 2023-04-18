f3a19acfce1054aebdc965bf1a777e59126fd7a1
Franco Bascialla <francobascialla@gmail.com>
2023-04-18 10:24:28 -0300

    initial commit

---
+++ UT4_Hernandez_Bascialla_Pijuan/src/main/java/com/mycompany/ut4_hernandez_bascialla_pijuan/UT4_Hernandez_Bascialla_Pijuan.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ut4_hernandez_bascialla_pijuan;
+
+/**
+ *
+ * @author Ale_macbook
+ */
+public class UT4_Hernandez_Bascialla_Pijuan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
