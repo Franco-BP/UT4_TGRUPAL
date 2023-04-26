fb18e7c3e814fa690940ab07ca490535bac66e4a
Franco Bascialla <francobascialla@gmail.com>
2023-04-26 11:41:20 -0300

    Se agrega metodo para guardar los numeros del archivo

--- UT4_Hernandez_Bascialla_Pijuan/src/main/java/com/mycompany/ut4_hernandez_bascialla_pijuan/Archivos.java
+++ UT4_Hernandez_Bascialla_Pijuan/src/main/java/com/mycompany/ut4_hernandez_bascialla_pijuan/Archivos.java
@@ -7,6 +7,7 @@ package com.mycompany.ut4_hernandez_bascialla_pijuan;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 /**
  *
@@ -45,6 +46,41 @@ public class Archivos {
                 e2.printStackTrace();
             }
         }
+    }
+    
+    public ArrayList<Integer> leerArchivosNumericos() {
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+        ArrayList<Integer> listaNumeros = new ArrayList<>();
+
+        try {
+            // Apertura del fichero y creacion de BufferedReader para poder
+            // hacer una lectura comoda (disponer del metodo readLine()).
+            archivo = new File("/Users/Ale_macbook/UT4_TGRUPAL/claves.txt");
+            fr = new FileReader(archivo);
+            br = new BufferedReader(fr);
+
+            // Lectura y guardado del fichero
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                listaNumeros.add(Integer.decode(linea));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta 
+            // una excepcion.
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return listaNumeros;
     }
 
 }
